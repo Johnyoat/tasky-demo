7e0af9fb6396c10d8ecec780b1f75fc951301ab8
Johnson Osei Yeboah <johnyoat@gmail.com>
2026-04-08 00:03:38 -0500

    feat(api): fiber init

--- tasky-api/cmd/taskyapi/main.go
+++ tasky-api/cmd/taskyapi/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
