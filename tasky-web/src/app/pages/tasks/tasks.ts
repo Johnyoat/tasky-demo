5db37c6fefaa5c1ab5b6cb503b91b4c9a2e6beff
Johnson Osei Yeboah <johnyoat@gmail.com>
2026-04-07 19:30:51 -0500

    feat(web): dummy tasks

--- tasky-web/src/app/pages/tasks/tasks.ts
+++ tasky-web/src/app/pages/tasks/tasks.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './tasks.css',
 })
 export class Tasks {
+  tasks: TaskModel[] = [
+    {id: '1', title: 'Task 1', description: 'Description 1', completed: false},
+    {id: '2', title: 'Task 2', description: 'Description 2', completed: true},
+    {id: '3', title: 'Task 3', description: 'Description 3', completed: false},
+    {id: '4', title: 'Task 4', description: 'Description 4', completed: false},
+  ];
 
+  constructor() {
+
+  }
 }
---
+++ tasky-web/src/models/task.model.ts
@@ -0,0 +1,7 @@
+
+interface TaskModel {
+  id: string;
+  title: string;
+  description: string;
+  completed: boolean;
+}
