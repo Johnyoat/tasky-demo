422ab4c08e4bbf6741d997d1d194c43c47796723
Johnson Osei Yeboah <johnyoat@gmail.com>
2026-04-07 19:09:12 -0500

    feat(web): task page init

--- tasky-web/src/app/app.routes.ts
+++ tasky-web/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {Tasks} from './pages/tasks/tasks';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'tasks', component: Tasks },
+  { path: '', component: Tasks, pathMatch: 'full' },
+];
